4a17c1ac1d2c4015e8033972c721197f71383a95
Dmytro <88747340+DmitriyMiroshnychenko@users.noreply.github.com>
2024-04-17 22:00:08 +0300

    added route

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 
@@ -5,7 +6,11 @@ function App() {
   return (
     <>
       <Header />
-      <main className="container content"></main>
+      <main className="container content">
+        <Router>
+          <Route path='/' component={}/>
+        </Router>
+      </main>
       <Footer />
     </>
   );
