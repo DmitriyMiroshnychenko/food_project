66de076d8da6ef4f3e5ce9bc5ced63a5fb13b133
Dmytro <88747340+DmitriyMiroshnychenko@users.noreply.github.com>
2024-04-22 18:58:19 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Footer } from './components/Footer';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Contact } from './pages/Contact';
+import { NotFound } from './pages/NotFound';
 
 function App() {
   return (
@@ -12,9 +13,12 @@ function App() {
       <Header />
       <main className="container content">
         <Router>
-          <Route path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contacts" component={Contact} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contacts" component={Contact} />
+            <Route component={NotFound} />
+          </Switch>
         </Router>
       </main>
       <Footer />
