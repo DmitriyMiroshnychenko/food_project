372b33d8599d0f15e0be4f4b4c4ddf786f37ed86
Dmytro <88747340+DmitriyMiroshnychenko@users.noreply.github.com>
2024-04-25 22:13:54 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,13 @@
 function Header() {
   return (
-    <nav className="blue darken-1">
+    <nav className="green darken-1">
       <div className="nav-wrapper">
         <a href="/" className="brand-logo">
           React Shop
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a
-              href="https://github.com/DmitriyMiroshnychenko/shop-project"
-              target="_blank"
-              rel="noreferer"
-            >
+            <a href="#!" target="_blank" rel="noreferrer">
               Nice
             </a>
           </li>
@@ -20,5 +16,4 @@ function Header() {
     </nav>
   );
 }
-
 export { Header };
